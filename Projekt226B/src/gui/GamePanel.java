88c8d346d1ce6526e0e7f2e19d59aef32a36d5a6
N.Hermann <noah.hermann@gibmit.ch>
2017-06-14 11:34:57 +0200

    commit

--- Projekt226B/src/gui/GamePanel.java
+++ Projekt226B/src/gui/GamePanel.java
@@ -13,25 +13,24 @@ import dto.Game;
 
 public class GamePanel extends JPanel {
 
-	
 	private JLabel titel = new JLabel();
-	
+
 	private JButton start = new JButton("Start");
 	private JButton bez = new JButton("Beschreibung");
-	
+
 	private Game g;
-	
+
 	public GamePanel(Game g) {
-		
+
 		this.g = g;
-		this.setLayout(new GridLayout(3,1));
-		if(this.g != null) {
-			titel.setText(this.g.getTitel()); 
+		this.setLayout(new GridLayout(3, 1));
+		if (this.g != null) {
+			titel.setText(this.g.getTitel());
 		}
 		this.add(titel);
 		this.add(start);
 		this.add(bez);
-		this.setBorder(new EmptyBorder(10,10,10,10));
+		this.setBorder(new EmptyBorder(10, 10, 10, 10));
 	}
-	
+
 }
